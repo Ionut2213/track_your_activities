849f36e09929f4befef925e099ca6d0d6c6451d5
laote <ihagiu1212@gmail.com>
2024-10-01 14:49:23 +0300

    i added the login and signup button, need to change the welcome msg

--- main_app.py
+++ main_app.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QHBoxLayout, QPushButton
 from PySide6.QtGui import QKeyEvent
 from PySide6.QtCore import Qt
 import sys
@@ -16,12 +16,34 @@ class FullScreenApp(QWidget):
 
         self.setStyleSheet("background-color: green;")
 
-        self.layout = QVBoxLayout()
+        # self.layout = QVBoxLayout()
 
-        self.central_label = QLabel("Welcome, Welcome !", self)
-        self.central_label.setStyleSheet("color: white; font-size:24px;")
-        self.central_label.setAlignment(Qt.AlignCenter)
-        self.layout.addWidget(self.central_label)
+        # Partea pentru butoanele signup/login
+
+        
+        self.layout = QHBoxLayout()
+
+        button_layout = QHBoxLayout()
+
+        self.login_button = QPushButton("Login", self)
+        self.signup_button = QPushButton("Signup", self)
+
+        self.login_button.setStyleSheet("background-color: white; color:black; font-size:18px;")
+        self.signup_button.setStyleSheet("background-color: white; color:black; font-size:18px")
+        button_layout.addWidget(self.login_button)
+        button_layout.addWidget(self.signup_button)
+        button_layout.setAlignment(Qt.AlignTop | Qt.AlignRight)
+        self.layout.addLayout(button_layout)
+        self.setLayout(self.layout)
+
+
+
+
+
+        # self.central_label = QLabel("Welcome, Welcome !", self)
+        # self.central_label.setStyleSheet("color: white; font-size:24px;")
+        # self.central_label.setAlignment(Qt.AlignCenter)
+        # self.layout.addWidget(self.central_label)
 
 
         self.setLayout(self.layout)
